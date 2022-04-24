bed205ce8b159b1a6cce4aaa814a3dc9f77cc069
yuvalkatzir <42411319+yuvalkatzir@users.noreply.github.com>
2022-04-24 12:42:41 +0300

    initialize agent

---
+++ agent/src/main/java/patam2/agent/HelloController.java
@@ -0,0 +1,14 @@
+package patam2.agent;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
