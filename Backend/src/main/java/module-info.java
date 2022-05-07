ac2e5f0a53a1726baef960752d7bf8472d9da549
yuvalkatzir <42411319+yuvalkatzir@users.noreply.github.com>
2022-05-07 14:48:42 +0300

    Migration to Gradle

--- Backend/src/main/java/module-info.java
+++ Backend/src/main/java/module-info.java
@@ -1,8 +1,8 @@
-module patam2.backend {
+module ptm2.backend {
     requires javafx.controls;
     requires javafx.fxml;
 
 
-    opens patam2.backend to javafx.fxml;
-    exports patam2.backend;
+    opens ptm2.backend to javafx.fxml;
+    exports ptm2.backend;
 }
