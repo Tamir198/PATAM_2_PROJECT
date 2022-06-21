e2cb7f9162d9e748a9b4d3b182bce643d3d3889b
yuvalkatzir <42411319+yuvalkatzir@users.noreply.github.com>
2022-06-21 22:16:25 +0300

    updated Add Flight window

--- Backend/src/sample/Main.java
+++ Backend/src/sample/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
 //  create a window
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Flight");
-        primaryStage.setScene(new Scene(root, 297, 353));
+        primaryStage.setTitle("Add Flight");
+        primaryStage.setScene(new Scene(root, 331, 364));
         primaryStage.show();
     }
 
